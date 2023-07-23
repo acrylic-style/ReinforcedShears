c7c4affd01d708baf95fa1160c23ad4e93a0edd2
acrylic-style <me@acrylicstyle.xyz>
2023-07-23 20:30:11 +0900

    balancing

--- src/main/java/xyz/acrylicstyle/shears/ReinforcedShears.java
+++ src/main/java/xyz/acrylicstyle/shears/ReinforcedShears.java
@@ -16,12 +16,15 @@ import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.player.PlayerShearEntityEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.RecipeChoice;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.ShapelessRecipe;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.Random;
 
 public class ReinforcedShears extends JavaPlugin implements Listener {
@@ -43,10 +46,15 @@ public class ReinforcedShears extends JavaPlugin implements Listener {
     public static ItemStack ui16384 = null;
     public static ItemStack diamond_shears_item = null;
 
-    public static ItemStack getItemStack(int level) {
+    public static @NotNull ItemStack getItemStack(int durability, int fortune, String... lore) {
         ItemStack item = new ItemStack(Material.SHEARS);
         ItemMeta meta = item.getItemMeta();
-        meta.addEnchant(Enchantment.DURABILITY, level, true);
+        assert meta != null;
+        meta.addEnchant(Enchantment.DURABILITY, durability, true);
+        if (fortune > 0) {
+            meta.addEnchant(Enchantment.LOOT_BONUS_BLOCKS, fortune, true);
+        }
+        meta.setLore(Arrays.asList(lore));
         item.setItemMeta(meta);
         return item;
     }
@@ -63,18 +71,19 @@ public class ReinforcedShears extends JavaPlugin implements Listener {
         u4096 = new NamespacedKey(this, "u4096");
         u16384 = new NamespacedKey(this, "u16384");
         diamond_shears = new NamespacedKey(this, "diamond_shears");
-        ui4 = getItemStack(4);
-        ui16 = getItemStack(16);
-        ui64 = getItemStack(64);
-        ui256 = getItemStack(256);
-        ui1024 = getItemStack(1024);
-        ui4096 = getItemStack(4096);
-        ui16384 = getItemStack(16384);
+        ui4 = getItemStack(4, 0, "(耐久が)4倍強化されたハサミ(1/7)");
+        ui16 = getItemStack(16, 1, "(耐久が)16倍強化されたハサミ(2/7)", "羊の羊毛を狩るときに羊毛が増えそう");
+        ui64 = getItemStack(64, 2, "(耐久が)64倍強化されたハサミ(3/7)", "羊の羊毛を狩るときに羊毛が増えそう");
+        ui256 = getItemStack(256, 3, "(耐久が)256倍強化されたハサミ(4/7)", "羊の羊毛を狩るときに羊毛が増えそう");
+        ui1024 = getItemStack(1024, 4, "(耐久が)1024倍強化されたハサミ(5/7)", "羊の羊毛を狩るときに羊毛が増えそう");
+        ui4096 = getItemStack(4096, 5, "(耐久が)4096倍強化されたハサミ(6/7)", "羊の羊毛を狩るときに羊毛が増えそう");
+        ui16384 = getItemStack(16384, 6, "(耐久が)16384倍強化されたハサミ(7/7)", "羊の羊毛を狩るときに羊毛が増えそう");
         diamond_shears_item = new ItemStack(Material.SHEARS);
         ItemMeta meta = diamond_shears_item.getItemMeta();
+        assert meta != null;
         meta.addEnchant(Enchantment.DIG_SPEED, 2, true);
         meta.addEnchant(Enchantment.LOOT_BONUS_BLOCKS, 3, true);
-        meta.setUnbreakable(true);
+        meta.addEnchant(Enchantment.DURABILITY, 3, true);
         meta.setDisplayName(ChatColor.AQUA + "ダイヤのハサミ");
         diamond_shears_item.setItemMeta(meta);
         ShapelessRecipe ur4 = new ShapelessRecipe(u4, ui4);
@@ -87,14 +96,14 @@ public class ReinforcedShears extends JavaPlugin implements Listener {
         ShapedRecipe diamond_shears_recipe = new ShapedRecipe(diamond_shears, diamond_shears_item);
         diamond_shears_recipe.shape(" D ", "DSD", " D ");
         diamond_shears_recipe.setIngredient('D', Material.DIAMOND);
-        diamond_shears_recipe.setIngredient('S', new ItemStack(Material.SHEARS));
-        ur4.addIngredient(4, new ItemStack(Material.SHEARS));
-        ur16.addIngredient(4, ui4);
-        ur64.addIngredient(4, ui16);
-        ur256.addIngredient(4, ui64);
-        ur1024.addIngredient(4, ui256);
-        ur4096.addIngredient(4, ui1024);
-        ur16384.addIngredient(4, ui4096);
+        diamond_shears_recipe.setIngredient('S', new RecipeChoice.ExactChoice(new ItemStack(Material.SHEARS)));
+        addIngredient(ur4, 4, new ItemStack(Material.SHEARS));
+        addIngredient(ur16, 4, ui4);
+        addIngredient(ur64, 4, ui16);
+        addIngredient(ur256, 4, ui64);
+        addIngredient(ur1024, 4, ui256);
+        addIngredient(ur4096, 4, ui1024);
+        addIngredient(ur16384, 4, ui4096);
         Bukkit.addRecipe(ur4);
         Bukkit.addRecipe(ur16);
         Bukkit.addRecipe(ur64);
@@ -103,6 +112,12 @@ public class ReinforcedShears extends JavaPlugin implements Listener {
         Bukkit.addRecipe(ur4096);
         Bukkit.addRecipe(ur16384);
         Bukkit.addRecipe(diamond_shears_recipe);
+    }
+
+    public static void addIngredient(ShapelessRecipe recipe, int count, ItemStack stack) {
+        for (int i = 0; i < count; i++) {
+            recipe.addIngredient(new RecipeChoice.ExactChoice(stack));
+        }
     }
 
     @Override
@@ -156,9 +171,12 @@ public class ReinforcedShears extends JavaPlugin implements Listener {
     }
 
     public void handleShear(@NotNull ItemStack item, @NotNull Location location, @NotNull String color) {
-        if (diamond_shears_item.isSimilar(item)) {
-            Item iteme = location.getWorld().spawn(location, Item.class);
-            iteme.setItemStack(new ItemStack(Material.valueOf(color + "_WOOL"), Math.max(1, random.nextInt(5))));
+        ItemMeta meta = item.getItemMeta();
+        if (meta == null) return;
+        int level = meta.getEnchantLevel(Enchantment.LOOT_BONUS_BLOCKS);
+        if (level > 0) {
+            Item iteme = Objects.requireNonNull(location.getWorld()).spawn(location, Item.class);
+            iteme.setItemStack(new ItemStack(Material.valueOf(color + "_WOOL"), random.nextInt(level) + 1));
         }
     }
 }
